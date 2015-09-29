61bb5f44d67fe98d5d994bbf60caa78f0ec9aca2
jsk389 <jsk389@bham.ac.uk>
2015-09-29 17:17:24 +0100

    Minor edits, change in calculation of cadence

--- Granulation/granulation.py
+++ Granulation/granulation.py
@@ -8,47 +8,47 @@ Create time series of granulation for a given rms amplitude (sigma) and
 characteristic timescale (tau). From De Ridder et al. 2006.
 """
 
-def granulation(t, sigma, tau):
+def granulation(time, sigma, tau):
     """
     Create granulation time series
     Usage:
-    t - array: time stamps
+    time - array: time stamps
     sigma - float: rms amplitude (ppm)
     tau - float: characteristic timescale (s)
     """
     
     # Compute cadence from time series - assuming regular sampling
-    cadence = t[1]-t[0]
+    dt = (time.max()-time.min()) / float(len(time))
 
     # Calculate white noise component
-    tempnoise = sigma * np.sqrt(cadence / tau) * \
-                np.random.randn(t.shape[0])
+    temp = sigma * np.sqrt(dt / tau) * \
+                np.random.randn(len(time))
 
-    anoise = np.zeros(t.shape[0])
-    anoise[0] = tempnoise[0]
+    anoise = np.zeros(len(time))
+    anoise[0] = temp[0]
     
     # Generate signal
-    for i in np.arange(t.shape[0])
-        anoise[i] = np.exp(-cadence/tau) * anoise[i-1] + tempnoise[i]
+    for i in range(len(time)):
+        anoise[i] = np.exp(-dt/tau) * anoise[i-1] + temp[i]
 
     return anoise
 
 if __name__=="__main__":
 
     # Quick demo script
-    cadence = 40.0
-    days = 365.0 * 1.0
-    npts = days * 24.0 * 3600.0 / cadence    
-    t = np.arange(0, npts*cadence, cadence)
+    dt = 40.0 # cadence
+    n_days = 365.0
+    N = (n_days * 86400.0) / dt    
+    time = np.linspace(0, N*dt, N)
 
     sigma = 52 #(ppm)
     tau = 206 # s
 
     # Generate time series
-    gran = granulation(t, sigma, tau)
+    gran = granulation(time, sigma, tau)
 
     # Plot
-    pl.plot(t, gran, 'k')
+    pl.plot(time, gran, 'k')
     pl.xlabel(r'Time (s)')
     pl.ylabel(r'Normalised Flux (ppm)')
     pl.show()
